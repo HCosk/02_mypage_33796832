3b4793263671be79a54d8f054a579aca2f4931a4
HCosk <151037558+HCosk@users.noreply.github.com>
2025-10-14 14:33:18 +0100

    updated work

--- Lab_1/index.js
+++ Lab_1/index.js
@@ -1,11 +1,38 @@
-var http = require("http"); 
-const port = 8000; 
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-http.createServer(function(req, res) { 
-    //console.log(req);
-    console.log("working changes")
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(" World!");
-    }).listen(port, function() { 
-        console.log(`Node server is running on port ${port}...`); 
-}); 
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  console.log("Server received a request...");
+
+  if (req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Error loading page");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/style.css") {
+    fs.readFile("style.css", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Error loading CSS");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Node server running on http://localhost:${port}`);
+});
