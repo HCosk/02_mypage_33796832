ed7caaf1b23cda64d9d1390f83f25b3ddfc36407
Hcosk <hakancoskun824@gmail.com>
2025-10-17 01:36:42 +0100

    minor updated javascript changes

--- index.js
+++ index.js
@@ -3,36 +3,38 @@ const fs = require("fs");
 const path = require("path");
 
 const port = 8000;
-console.log("hello")
-const server = http.createServer((req, res) => {
-  console.log("Server received a request...");
 
-  if (req.url === "/") {
-    fs.readFile("index.html", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error loading page");
+const mimeTypes = {
+  ".html": "text/html",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".jpeg": "image/jpeg",
+  ".json": "application/json"
+};
+
+const server = http.createServer((req, res) => {
+  console.log("Server received a request:", req.url);
+
+  let filePath = req.url === "/" ? "index.html" : req.url.substring(1);
+  let extname = path.extname(filePath).toLowerCase();
+  let contentType = mimeTypes[extname] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Not Found");
       } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      }
-    });
-  } else if (req.url === "/style.css") {
-    fs.readFile("style.css", (err, data) => {
-      if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error loading CSS");
-      } else {
-        res.writeHead(200, { "Content-Type": "text/css" });
-        res.end(data);
+        res.end("500 Internal Server Error");
       }
-    });
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
-  }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, () => {
-  console.log(`Node server running on http://localhost:${port}`);
+  console.log(`Node server running on ${port}`);
 });
